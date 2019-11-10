c0c162554f83c8ee65bfed119034343a7dfb3dcb
flumpool2223 <spitz.us.flumpool@gmail.com>
2019-11-10 16:35:01 +0900

    chat-space_Automatic-updating_完成①

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,9 @@
-$(document).on ('turbolinks:load', function(){ 
+$(document).on('turbolinks:load', function(){ 
   function buildHTML(message){
-
-    var image = message.image ?  `<img src=${message.image} > ` : '';
+    var image = message.image.url ?  `<img src=${message.image.url} > ` : '';
 
      var html =
-      `<div class="message" data-message-id=${message.id}>
+      `<div class="message" data-message_id=${message.id}>
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -22,31 +21,57 @@ $(document).on ('turbolinks:load', function(){
        </div>`
      return html;
  }
-$('.new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
 
- $('#chat-footer__send-btn').removeAttr('data-disable-with');
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
 
- var url = $(this).attr('action')
+    $('#chat-footer__send-btn').removeAttr('data-disable-with');
 
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-    $('form')[0].reset();
-  })
-  .fail(function(){
-    alert('error');
+    var url = $(this).attr('action')
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+        $('form')[0].reset();
+      })
+      .fail(function(){
+        alert('error');
+      });
+      return false;
   });
-  return false;
-});
+
+
+  var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  var last_message_id = $('.message:last').data("message_id");
+    $.ajax({
+      url: "./api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {last_id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function(message) {
+        insertHTML = buildHTML(message);
+      $('.messages').append(insertHTML);
+    })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  }
+};
+setInterval(reloadMessages, 5000);
 });
